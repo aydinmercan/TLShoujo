ad92b0c95fc39f796274c5381f6723d45901a652
Aydin Mercan <aydin@mercan.dev>
2021-03-30 18:09:06 +0300

    temporarily use gcc/clang builtins for locking

---
+++ include/internal/core.h
@@ -0,0 +1,4 @@
+#pragma once
+
+void _core_lock(void);
+void _core_unlock(void);
--- src/core.c
+++ src/core.c
@@ -1,30 +1,26 @@
 #include "tlshoujo/core.h"
 
+#include "internal/core.h"
 #include "internal/impl.h"
 #include "tlshoujo/probe.h"
 
-extern void _shoujo_core_lock(void);
-extern void _shoujo_core_unlock(void);
-
 static volatile int _initialized = 0;
 
-volatile int _core_lock;
-
-int __attribute__((flatten)) shoujo_init(void) {
-    _shoujo_core_lock();
+int shoujo_init(void) {
+    _core_lock();
 
     if (_initialized != 0) {
         return 1;
     }
 
     if (shoujo_probe_cpu_features() != 0) {
-        _shoujo_core_unlock();
+        _core_unlock();
         return -1;
     }
 
     _initialized = 1;
 
-    _shoujo_core_unlock();
+    _core_unlock();
 
     return 0;
 }
